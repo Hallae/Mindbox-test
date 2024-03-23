9301bdba8671fd21b23af80f637801f34385e712
Hallae <mr.tabby@hotmail.com>
2024-03-24 01:22:59 +0300

    feat:Unit tests

---
+++ CircleTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using ShapeAreaCalculator;
+
+namespace ShapeAreaCalculatorTests
+{
+    [TestFixture]
+    public class CircleTests
+    {
+        [Test]
+        public void GetArea_ReturnsCorrectArea()
+        {
+            // Arrange
+            var circle = new Circle { Radius = 5 };
+
+            // Act
+            var result = circle.GetArea();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(78.53981633974483).Within(0.000000000000001)); // πr² for r=5
+        }
+    }
+}
---
+++ TriangleTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using ShapeAreaCalculator;
+
+namespace ShapeAreaCalculatorTests
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [Test]
+        public void GetArea_ReturnsCorrectArea()
+        {
+            // Arrange
+            var triangle = new Triangle { Side1 = 3, Side2 = 4, Side3 = 5 };
+
+            // Act
+            var result = triangle.GetArea();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(6).Within(0.000000000000001)); // Heron's formula for sides 3, 4, 5
+        }
+
+        [Test]
+        public void IsRectangular_ReturnsTrueForRightAngledTriangle()
+        {
+            // Arrange
+            var triangle = new Triangle { Side1 = 3, Side2 = 4, Side3 = 5 };
+
+            // Act
+            var result = triangle.IsRectangular();
+
+            // Assert
+            Assert.That(result, Is.True); // 3, 4, 5 form a right-angled triangle
+        }
+
+        [Test]
+        public void IsRectangular_ReturnsFalseForNonRightAngledTriangle()
+        {
+            // Arrange
+            var triangle = new Triangle { Side1 = 3, Side2 = 4, Side3 = 6 };
+
+            // Act
+            var result = triangle.IsRectangular();
+
+            // Assert
+            Assert.That(result, Is.False); // 3, 4, 6 do not form a right-angled triangle
+        }
+    }
+}
