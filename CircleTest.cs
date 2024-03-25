f0f2866b7a2821cb4cca81a4841531c42447cc32
Hallae <mr.tabby@hotmail.com>
2024-03-26 00:21:36 +0300

    update: comment

--- CircleTest.cs
+++ CircleTest.cs
@@ -6,17 +6,20 @@ namespace ShapeAreaCalculatorTests
     [TestFixture]
     public class CircleTests
     {
+        // Test to verify that the GetArea method of the Circle class returns the correct area for a circle with a radius of 5.
         [Test]
-        public void GetArea_ReturnsCorrectArea()
+        public void GetArea_WithRadius5_ReturnsCorrectArea()
         {
-            // Arrange
+            // Arrange: Set up the test by creating a circle with a radius of 5.
             var circle = new Circle { Radius = 5 };
 
-            // Act
+            // Act: Call the GetArea method on the circle.
             var result = circle.GetArea();
 
-            // Assert
-            Assert.That(result, Is.EqualTo(78.53981633974483).Within(0.000000000000001)); // πr² for r=5
+            // Assert: Verify that the result is equal to the expected area of a circle with a radius of 5.
+            // The expected area is calculated as πr², where r = 5.
+            var expectedArea = Math.PI * Math.Pow(5, 2);
+            Assert.That(result, Is.EqualTo(expectedArea).Within(0.000000000000001));
         }
     }
 }
